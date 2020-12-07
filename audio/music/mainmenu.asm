939bf7104bbc47c869a21ca8d59f7fe72431de95
lvl-3-g <60635528+lvl-3-g@users.noreply.github.com>
2020-12-07 10:22:54 -0500

    v3.12
    - fixed main menu music to also be in stereo, missed that one initially

--- audio/music/mainmenu.asm
+++ audio/music/mainmenu.asm
@@ -1,181 +1,145 @@
 Music_MainMenu:
-	channel_count 4
-	channel 1, start03_Ch1
-	channel 2, start03_Ch2
-	channel 3, start03_Ch3
-	channel 4, start03_Ch4
-;	include	group.def
-;bank3d	group	G_MUSIC4
-;
-;;	field03
-;
-;;	Converting on Thu May 27 07:58:32 1999
-;
-;;				by ver 1.00
-;
-;	public	musstart
-;musstart:
+	musicheader 4, 1, Music_MainMenu_Ch1
+	musicheader 1, 2, Music_MainMenu_Ch2
+	musicheader 1, 3, Music_MainMenu_Ch3
+	musicheader 1, 4, Music_MainMenu_Ch4
 
-
-
-
-
-
-
-
-;----------------------------------------
-start03_Ch1:
-;----------------------------------------
+Music_MainMenu_Ch1:
 	tempo 160
-	volume 7, 7
-	duty_cycle 3
-	pitch_offset 1
-	vibrato 16, 1, 5
-; P1-1
-	note_type 12, 0, 15
+	volume $77
+	dutycycle $3
+	tone $0001
+	vibrato $10, $15
+	stereopanning $f0
+	pitchoffset 0, 2
+	notetype $c, $f
 	octave 3
-	note D#,8
-.loop10
-	note_type 12, 9, 2
+	note D_, 8
+Music_MainMenu_branch_f791d:
+	notetype $c, $92
 	octave 3
-	note D_,6
-	note D_,1
-	note D_,1
-	note D_,2
-	note D_,2
-	note D_,4
-	db sound_loop_cmd, 2
-	dw .loop10
-; P1-2
-.loop11
-	note E_,6
-	note E_,1
-	note E_,1
-	note E_,2
-	note E_,2
-	note E_,4
-	note C_,6
-	note C_,1
-	note C_,1
-	note C_,2
-	note C_,2
-	note_type 12, 9, 4
-	note C#,4
-	db sound_loop_cmd, 0
-	dw .loop10
-;----------------------------------------
-start03_Ch2:
-;----------------------------------------
-	duty_cycle 3
-; P2-1
-	note_type 12, 0, 15
-	octave 3
-	note G#,8
-.loop20
-	note_type 12, 10, 2
-	octave 3
-	note G_,6
-	note G_,1
-	note G_,1
-	note G_,2
-	note G_,2
-	note G_,4
-	db sound_loop_cmd, 2
-	dw .loop20
-.loop21
-	octave 3
-	note A_,6
-	note A_,1
-	note A_,1
-	note A_,2
-	note A_,2
-	note A_,4
-	octave 3
-	note F_,6
-	note F_,1
-	note F_,1
-	note F_,2
-	note F_,2
-	note_type 12, 10, 4
-	note F#,4
-	db sound_loop_cmd, 0
-	dw .loop20
-;----------------------------------------
-start03_Ch3:
-;----------------------------------------
-; P3-1
-	note_type 12, 2, 5
-	rest 8
-.loop30
+	note C#, 6
+	note C#, 1
+	note C#, 1
+	note C#, 2
+	note C#, 2
+	note C#, 4
+	loopchannel 2, Music_MainMenu_branch_f791d
+	note D#, 6
+	note D#, 1
+	note D#, 1
+	note D#, 2
+	note D#, 2
+	note D#, 4
 	octave 2
-	note G_,1
-	rest 5
-	note G_,1
-	note G_,1
-	note G_,1
-	rest 1
-	note G_,1
-	rest 1
-	note G_,1
-	rest 3
-	db sound_loop_cmd, 2
-	dw .loop30
-; P3-2
-.loop31
-	note A_,1
-	rest 5
-	note A_,1
-	note A_,1
-	note A_,1
-	rest 1
-	note A_,1
-	rest 1
-	note A_,1
-	rest 3
-	note F_,1
-	rest 5
-	note F_,1
-	note F_,1
-	note F_,1
-	rest 1
-	note F_,1
-	rest 1
-	note F#,1
-	rest 3
-	db sound_loop_cmd, 0
-	dw .loop30
-;----------------------------------------
-start03_Ch4:
-;----------------------------------------
-	toggle_noise 3
-; P4-1
-	drum_speed 12
-	rest 8
-	rest 16
-	rest 16
-	rest 16
-	rest 12
-	drum_note 3,1
-	drum_note 3,1
-	drum_note 3,1
-	drum_note 3,1
-.loop40
-	drum_note 3,6
-	drum_note 3,1
-	drum_note 3,1
-	drum_note 3,2
-	drum_note 3,2
-	drum_note 3,4
-	drum_note 3,6
-	drum_note 3,1
-	drum_note 3,1
-	drum_note 3,2
-	drum_note 3,2
-	drum_note 4,1
-	drum_note 4,1
-	drum_note 3,1
-	drum_note 3,1
-	db sound_loop_cmd, 0
-	dw .loop40
+	note B_, 6
+	note B_, 1
+	note B_, 1
+	note B_, 2
+	note B_, 2
+	notetype $c, $94
+	octave 3
+	note C_, 4
+	loopchannel 0, Music_MainMenu_branch_f791d
 
+Music_MainMenu_Ch2:
+	dutycycle $3
+	stereopanning $f
+	notetype $c, $f
+	pitchoffset 0, 2
+	octave 3
+	note G_, 8
+Music_MainMenu_branch_f7949:
+	notetype $c, $a2
+	note F#, 6
+	note F#, 1
+	note F#, 1
+	note F#, 2
+	note F#, 2
+	note F#, 4
+	loopchannel 2, Music_MainMenu_branch_f7949
+	note G#, 6
+	note G#, 1
+	note G#, 1
+	note G#, 2
+	note G#, 2
+	note G#, 4
+	note E_, 6
+	note E_, 1
+	note E_, 1
+	note E_, 2
+	note E_, 2
+	notetype $c, $a4
+	note F_, 4
+	loopchannel 0, Music_MainMenu_branch_f7949
 
+Music_MainMenu_Ch3:
+	stereopanning $f0
+	notetype $c, $25
+	pitchoffset 0, 2
+	note __, 8
+Music_MainMenu_branch_f796f:
+	octave 2
+	note F#, 1
+	note __, 5
+	note F#, 1
+	note F#, 1
+	note F#, 1
+	note __, 1
+	note F#, 1
+	note __, 1
+	note F#, 1
+	note __, 3
+	loopchannel 2, Music_MainMenu_branch_f796f
+	note G#, 1
+	note __, 5
+	note G#, 1
+	note G#, 1
+	note G#, 1
+	note __, 1
+	note G#, 1
+	note __, 1
+	note G#, 1
+	note __, 3
+	note E_, 1
+	note __, 5
+	note E_, 1
+	note E_, 1
+	note E_, 1
+	note __, 1
+	note E_, 1
+	note __, 1
+	note F_, 1
+	note __, 3
+	loopchannel 0, Music_MainMenu_branch_f796f
+
+Music_MainMenu_Ch4:
+	stereopanning $f
+	togglenoise $3
+	notetype $c
+	note __, 8
+	note __, 16
+	note __, 16
+	note __, 16
+	note __, 12
+	note D_, 1
+	note D_, 1
+	note D_, 1
+	note D_, 1
+Music_MainMenu_branch_f79a5:
+	note D_, 6
+	note D_, 1
+	note D_, 1
+	note D_, 2
+	note D_, 2
+	note D_, 4
+	note D_, 6
+	note D_, 1
+	note D_, 1
+	note D_, 2
+	note D_, 2
+	note D#, 1
+	note D#, 1
+	note D_, 1
+	note D_, 1
+	loopchannel 0, Music_MainMenu_branch_f79a5
